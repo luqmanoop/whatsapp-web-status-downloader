d2e230063f3cdbfc8361320b06ce21438425bc4d
Luqman Olushi <olushilukmon03@gmail.com>
2019-09-06 10:33:29 +0100

    broadcast message to content script

---
+++ backgroud.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      message: 'process_download'
+    });
+  });
+});
